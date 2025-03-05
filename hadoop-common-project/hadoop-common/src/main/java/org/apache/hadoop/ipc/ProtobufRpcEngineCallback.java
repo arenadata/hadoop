05db1e77ca6ddeb436b9b94e7e65a6d1b93a6f57
Andrey Shitov <sai@arenadata.io>
2025-03-05 16:04:23 +0300

    fix(protobuf-2.5.0): remove 2.5.0 dependency.

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/ProtobufRpcEngineCallback.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/ProtobufRpcEngineCallback.java
@@ -18,7 +18,7 @@
 
 package org.apache.hadoop.ipc;
 
-import com.google.protobuf.Message;
+import   org.apache.hadoop.thirdparty.protobuf.Message;
 
 /**
  *  This engine uses Protobuf 2.5.0. Recommended to upgrade to Protobuf 3.x
